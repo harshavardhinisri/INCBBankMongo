082cabecc5c0d8cc6726c4871f51eb8c8b2a5bdc
prashant9912 <prashantg9912@gmail.com>
2021-09-22 22:43:07 +0530

    changes

--- src/main/java/com/barclays/rest/works/entity/Employee.java
+++ src/main/java/com/barclays/rest/works/entity/Employee.java
@@ -7,20 +7,59 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Employee {
 	@Id
 	private int empId;
-	private String empName; 
+	private String password;
+	private String email;
+	private String empName;
+	private String pancard;
+	private String aadhar;
 	private double salary;
 	private Account account;
 	
 	public Employee() {}
 	
-	public Employee(int empId, String empName, double salary, Account account ) {
+	public Employee(int empId, String empName,String password,String pancard,String aadhar,String email, double salary, Account account ) {
 		super();
 		this.empId = empId;
+		this.password = password;
 		this.empName = empName;
 		this.salary = salary;
+		this.email = email;
+		this.pancard = pancard;
+		this.aadhar = aadhar;
 		this.account = account;
 	}
 
+	public String getPancard() {
+		return pancard;
+	}
+
+	public String getAadhar() {
+		return aadhar;
+	}
+
+	public void setPancard(String pancard) {
+		this.pancard = pancard;
+	}
+
+	public void setAadhar(String aadhar) {
+		this.aadhar = aadhar;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public Account getAccount() {
 		return account;
