1630f5dddda91dbe072d6863d38a01456330a944
prashant9912 <prashantg9912@gmail.com>
2021-09-22 22:58:55 +0530

    CORS Issue

--- src/main/java/com/barclays/rest/works/config/CORSFilter.java
+++ src/main/java/com/barclays/rest/works/config/CORSFilter.java
@@ -1,18 +1,47 @@
-
-import javax.servlet.*;
-import javax.servlet.annotation.*;
+package com.barclays.rest.works.config;
 import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
-@WebFilter(filterName = "CORSFilter")
+@Component
 public class CORSFilter implements Filter {
-    public void init(FilterConfig config) throws ServletException {
-    }
 
-    public void destroy() {
+    private final Logger log = LoggerFactory.getLogger(CORSFilter.class);
+
+    public CORSFilter() {
+        log.info("SimpleCORSFilter init");
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
-        chain.doFilter(request, response);
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+
+        chain.doFilter(req, res);
     }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
 }
